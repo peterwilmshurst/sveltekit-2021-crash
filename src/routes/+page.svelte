a4f927732499ae0c62345ee81599c06669f0f386
peterwilmshurst <peterwilmshurst@hotmail.com>
2022-08-30 23:01:43 +0100

    styling the pokemon cards

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,12 +1,14 @@
 <script>
     import {pokemon} from '../stores/pokestore'
-    console.log($pokemon);
+    import PokemanCard from '../components/pokemanCard.svelte'
 </script>
 <svelte:head>
     <title>Sveltekit Pokedex</title>
 </svelte:head>
 <h1 class='text-4xl text-center my-8 uppercase'>SvelteKit Pokedex</h1>
 
+<div class='py-4 grid gap-4 md:grid-cols-2 grid-cols-1'>
 {#each $pokemon as pokeman}
-<p>{pokeman.name}</p>
+<PokemanCard pokeman={pokeman} />
 {/each}
+</div>
